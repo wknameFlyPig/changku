4fa86e000dd7baddf1557a74a3b891db87134761
wknameFlyPig <691164960@qq.com>
2016-12-02 17:19:23 +0800

    新建工程

---
+++ dada/dada/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  dada
+//
+//  Created by tarena013 on 16/12/2.
+//  Copyright © 2016年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ dada/dada/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  dada
+//
+//  Created by tarena013 on 16/12/2.
+//  Copyright © 2016年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
